08d899e106103da3839a9c1a6a338eb3959c5259
igor silvestrin <silvestrinigor@icloud.com>
2025-01-03 14:16:41 -0300

    c

--- axis/vapix/apis.py
+++ axis/vapix/apis.py
@@ -505,6 +505,25 @@ class RequestTimeApi(RequestAxisVapix):
     def get_suported_versions(self):
         return super()._get_supported_versions()
 
+class RequestNtpApi(RequestAxisVapix):
+    """
+    API Discovery: id=ntp
+    Property: Properties.API.HTTP.Version=3
+    Firmware: 9.10 and later
+    """
+    def __init__(self, host, port, api_version = None, context = None):
+        super().__init__(host, port, api_version, context)
+        self._api_path_type = ApiPathType.AXIS_CGI_NTP.value
+        
+    def get_ntp_info(self): # TODO: Implement this function
+        raise NotImplementedError("This function is not implemented yet.")
+        
+    def set_ntp_client_configuration(self): # TODO: Implement this function
+        raise NotImplementedError("This function is not implemented yet.")
+
+    def get_supported_versions(self):
+        return super()._get_supported_versions()
+
 class RequestVideoStreaming(RequestAxisVapix): # TODO: Implement this class
     """
     Property: Properties.API.HTTP.Version=3
