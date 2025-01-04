276ac2411f726a78355af6636d70cc94d965ec15
Igor Silvestrin <silvestrinigor@icloud.com>
2025-01-04 10:22:52 -0300

    c

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 import axis.vapix
 import axis.vapix.apis.time_api
 import axis.vapix.utils
-from tests_axis import HOST, PORT, PASSWORD, USERNAME
+from tests import HOST, PORT, PASSWORD, USERNAME
 
 
