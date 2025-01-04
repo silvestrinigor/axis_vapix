a5cc8a9e6e330fc66fb5026d241455da3256b472
Igor Silvestrin <silvestrinigor@icloud.com>
2025-01-04 16:54:00 -0300

    fix exeptions string message

--- axis/vapix/exeptions.py
+++ axis/vapix/exeptions.py
@@ -5,39 +5,39 @@ class AxisVapixException(Exception):
 
 class AxisVapixVersionNotSupportedException(AxisVapixException): 
     def __init__(self):
-        super().__init__("The specified version is not supported.")
+        super().__init__("The specified version is not supported")
 
 class AxisVapixInternalErrorException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Internal error.")
+        super().__init__("Internal error")
 
 class AxisVapixInvalidRequestException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Invalid request.")
+        super().__init__("Invalid request")
 
 class AxisVapixRequestBodyTooLargeException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Request body too large.")
+        super().__init__("Request body too large")
 
 class AxisVapixInvalidJsonDataException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Invalid JSON data.")
+        super().__init__("Invalid JSON data")
 
 class AxisVapixMethodDoesNotExistException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Method does not exist.")
+        super().__init__("Method does not exist")
 
 class AxisVapixAuthorizationFailedException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Authorization failed.")
+        super().__init__("Authorization failed")
 
 class AxisVapixMissingParameterException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Missing parameter(s).")
+        super().__init__("Missing parameter(s)")
 
 class AxisVapixInvalidParameterException(AxisVapixException): 
     def __init__(self):
-        super().__init__("Invalid parameter(s).")
+        super().__init__("Invalid parameter(s)")
 
 code_exeptions: dict[int, type[AxisVapixException]] = {
     2000: AxisVapixInvalidRequestException,
