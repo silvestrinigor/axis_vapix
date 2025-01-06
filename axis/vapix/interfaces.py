30cab232dbb84ee7c757def641352c2763761230
igor silvestrin <silvestrinigor@icloud.com>
2025-01-06 17:47:47 -0300

    added classes for future async implementation

--- axis/vapix/interfaces.py
+++ axis/vapix/interfaces.py
@@ -26,3 +26,28 @@ class IRequestAxisVapix(ABC):
 
     def _create_request(self, method=None, url=None, headers=None, files=None, data=None, params=None, auth=None, cookies=None, hooks=None, json=None):
         return AxisRequest(method, url, headers, files, data, params, auth, cookies, hooks, json)
+
+class IRequestAsyncAxisVapix(ABC):
+    def __init__(self, host: str, port: int, api_version: ApiVersion | None = None, context: str | None = None):
+        self._host = host
+        self._port = port
+        self._api_version = api_version
+        self._api_path_type = ApiPathType.NONE
+        self._context = context
+
+    def _get_basic_request_body(self):
+        request_body = {}
+        if self._api_version != None: request_body[RequestParamType.API_VERSION.value] = str(self._api_version)
+        if self._context != None: request_body[RequestParamType.CONTEXT.value] = self._context 
+        return request_body
+
+    def _get_supported_versions(self):
+        request_body = self._get_basic_request_body()
+        request_body[RequestParamType.METHOD.value] = MethodType.GET_SUPPORTED_VERSIONS.value
+        if self._api_path_type.value == None: 
+            raise ValueError("API path type is not set")
+        return self._create_request("POST", f"http://{self._host}:{self._port}/{self._api_path_type.value}", json= request_body)
+
+    def _create_request(self): # TODO: Implement this function
+        raise NotImplementedError("This function is not implemented yet.")
+    
--- axis/vapix/request.py
+++ axis/vapix/request.py
@@ -1,5 +1,12 @@
+from aiohttp import ClientRequest, http
 from requests import Request
 
 class AxisRequest(Request):
     def __init__(self, method=None, url=None, headers=None, files=None, data=None, params=None, auth=None, cookies=None, hooks=None, json=None):
         super().__init__(method, url, headers, files, data, params, auth, cookies, hooks, json)
+
+class AxisRequestAsync(ClientRequest):
+    def __init__(self, method, url, *, params = None, headers = None, skip_auto_headers = None, data = None, cookies = None, auth = None, version = http.HttpVersion11, compress = None, chunked = None, expect100 = False, loop = None, response_class = None, proxy = None, proxy_auth = None, timer = None, session = None, ssl = True, proxy_headers = None, traces = None, trust_env = False, server_hostname = None):
+        super().__init__(method, url, params=params, headers=headers, skip_auto_headers=skip_auto_headers, data=data, cookies=cookies, auth=auth, version=version, compress=compress, chunked=chunked, expect100=expect100, loop=loop, response_class=response_class, proxy=proxy, proxy_auth=proxy_auth, timer=timer, session=session, ssl=ssl, proxy_headers=proxy_headers, traces=traces, trust_env=trust_env, server_hostname=server_hostname)
+        
+        
