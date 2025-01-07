335805643f531f13ed4b555e2cf0752e87aed080
igor silvestrin <silvestrinigor@icloud.com>
2025-01-06 23:51:47 -0300

    implemating async request

--- axis/vapix/apis/api_discovery_service.py
+++ axis/vapix/apis/api_discovery_service.py
@@ -2,13 +2,27 @@
 https://developer.axis.com/vapix/network-video/api-discovery-service
 """
 
-from ..interfaces import IRequestAxisVapix
+from ..interfaces import IRequestAxisVapix, IRequestAsyncAxisVapix
 from ..types import ApiPathType, RequestParamType, MethodType
 from ..params import ApiVersion, FirmwareVersion
 
 API_DISCOVERY_SERVICE_LOWER_FIRMWARE_VERSION_SUPPORTED = FirmwareVersion(8, 50, 0)
 
 class RequestApiDiscoveryService(IRequestAxisVapix):
+
+    def __init__(self, host: str, port: int, api_version: ApiVersion, context: str | None = None):
+        super().__init__(host, port, api_version, context)
+        self._api_path_type = ApiPathType.AXIS_CGI_API_DISCOVERY
+
+    def get_api_list(self):
+        request_body = self._get_basic_request_body()
+        request_body[RequestParamType.METHOD.value] = MethodType.GET_API_LIST.value
+        return self._create_request("POST", f"http://{self._host}:{self._port}/{self._api_path_type.value}", json= request_body)
+
+    def get_supported_versions(self):
+        return super()._get_supported_versions()
+
+class RequestAsyncApiDiscoveryService(IRequestAsyncAxisVapix):
 
     def __init__(self, host: str, port: int, api_version: ApiVersion, context: str | None = None):
         super().__init__(host, port, api_version, context)
