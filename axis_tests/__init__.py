6e8627d288a31c30cbbed5a810d074233d15737f
igor silvestrin <silvestrinigor@icloud.com>
2025-01-08 17:04:06 -0300

    adding logging in tests

--- axis_tests/__init__.py
+++ axis_tests/__init__.py
@@ -1,6 +1,13 @@
-import sys
 import os
 from dotenv import load_dotenv
+import logging
+
+# Set up logging configuration
+logging.basicConfig(
+    filename='app.log',  # Specify the log file name
+    level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Customize the log message format
+)
 
 load_dotenv()
 HOST=os.getenv("HOST")
