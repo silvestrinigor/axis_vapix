439cfde4c8dc09677a49622eae75a3b3802b90fc
Igor Silvestrin <silvestrinigor@icloud.com>
2025-01-07 22:12:27 -0300

    added async functions

--- axis/vapix/apis/basic_device_information.py
+++ axis/vapix/apis/basic_device_information.py
@@ -5,6 +5,7 @@ https://developer.axis.com/vapix/network-video/basic-device-information
 from ..interfaces import IRequestAxisVapix
 from ..types import ApiPathType, DevicePropertyType, ParamType, RequestParamType, MethodType
 from ..params import ApiVersion, FirmwareVersion
+from .. import request
 
 BASIC_DEVICE_INFORMATION_API_LOWER_FIRMWARE_VERSION_SUPPORTED = FirmwareVersion(8, 40, 0)
 BASIC_DEVICE_INFORMATION_API_DISCOVERY_API_ID = "basic-device-info"
@@ -34,3 +35,21 @@ class RequestBasicDeviceInformation(IRequestAxisVapix):
 
     def get_supported_versions(self):
         return super()._get_supported_versions()
+
+    async def get_properties_async(self, properties: list[DevicePropertyType], session: request.AxisVapixAsyncSession):
+        params = {ParamType.PROPERTY_LIST.value: [prop.value for prop in properties]}
+        request_body = self._get_basic_request_body()
+        request_body[RequestParamType.METHOD.value] = MethodType.GET_PROPERTIES.value
+        request_body[RequestParamType.PARAMS.value] = params
+        return await session.post(f"http://{self._host}:{self._port}/{self._api_path_type.value}", json= request_body, session=session, auth=session.auth)
+    
+    async def get_all_properties_async(self, session: request.AxisVapixAsyncSession):
+        request_body = self._get_basic_request_body()
+        request_body[RequestParamType.METHOD.value] = MethodType.GET_ALL_PROPERTIES.value
+        return await session.post(f"http://{self._host}:{self._port}/{self._api_path_type.value}", json= request_body, session=session, auth=session.auth)
+    
+    async def get_all_unrestricted_properties_async(self, session: request.AxisVapixAsyncSession):
+        request_body = self._get_basic_request_body()
+        request_body[RequestParamType.METHOD.value] = MethodType.GET_ALL_UNRESTRICTED_PROPERTIES.value
+        return await session.post(f"http://{self._host}:{self._port}/{self._api_path_type.value}", json= request_body, session=session, auth=session.auth)
+    
