6921a30cb0f54e736bcd211778c2ec8980fd60a8
igor silvestrin <silvestrinigor@icloud.com>
2025-04-04 13:48:50 -0300

    update

--- axis/vapix/requests.py
+++ axis/vapix/requests.py
@@ -1,11 +1,16 @@
 from requests.auth import AuthBase
+from packaging.version import Version
 
 class VapixApiRequest:
     API_PATH = ""
     
-    def __init__(self, host: str, port: int, auth: AuthBase | None = None, secure: bool = False, api_version: str = "1.0", context: str = ""):
+    def __init__(self, host: str, port: int, auth: AuthBase | None = None, secure: bool = False, api_version: str | Version = "1.0", context: str = ""):
         protocol = "https" if secure else "http"
+        
+        if isinstance(api_version, Version):
+            api_version = str(api_version)
         self.api_version = api_version
+        
         self.context = context
         self.auth = auth
         self.url = f"{protocol}://{host}:{port}/{self.API_PATH}"
